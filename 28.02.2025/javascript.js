f501e5ddd10592356bf7627d2e605ecc88da12fb
Alisa2929 <alisastanevica@gmail.com>
2025-02-28 09:27:03 +0200

    28.02.2025

---
+++ 28.02.2025/javascript.js
@@ -0,0 +1,113 @@
+
+let m = "marts"; 
+
+switch(m)
+{
+    case "janvāris": 
+    console.log("ziema");
+    break;
+    case "februāris":
+    console.log("ziema");
+    break;
+    case "marts":
+    console.log("pavasaris");
+    break;
+    case "aprīlis":
+    console.log("pavasaris");
+    break;
+    case "maijs":
+    console.log("pavasaris");
+    break;
+    case "jūnijs":
+    console.log("vasara");
+    break;
+    case "jūlijs":
+    console.log("vasara");
+    break;
+    case "augusts":
+    console.log("vasara");
+    break;
+    case "septembris":
+    console.log("rudens");
+    break;
+    case "oktobris":
+    console.log("rudens");
+    break;
+    case "novembris":
+    console.log("rudens");
+    break;
+    case "decembris":
+    console.log("ziema");
+    break;
+}
+
+
+let diena = "sestdiena"; 
+
+switch(diena)
+{
+    case "pirmdiena": 
+    console.log("darba diena");
+    break;
+    case "otrdiena":
+    console.log("darba diena");
+    break;
+    case "trešdiena":
+    console.log("darba diena");
+    break;
+    case "ceturtdiena":
+    console.log("darba diena");
+    break;
+    case "piektdiena":
+    console.log("darba diena");
+    break;
+    case "sestdiena":
+    console.log("brīvdiena");
+    break;
+    case "svētdiena":
+    console.log("brīvdiena");
+    break;
+}
+
+
+let a = "7"; 
+
+switch(a)
+{
+    case "10": 
+    case "9":
+        case "8":
+    console.log("Labi");
+    break;
+    case "7":
+        case "6":
+            case "5":
+    console.log("Vidēji");
+    break;
+
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+                    console.log("Slikti");
+    break;
+    
+}
+
+let valutas = "EUR"; 
+
+switch(valutas)
+{
+    case "USD": 
+    console.log("Amerikas dolārs");
+    break;
+    case "EUR":
+    console.log("Eiro");
+    break;
+
+    case "GBP":
+    console.log("Lielbritānijas mārciņa");
+    break;
+    
+}
+    
