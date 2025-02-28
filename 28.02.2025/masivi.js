276080cb4b71fb68ed9042c5130444b9e6d1e69d
Alisa2929 <alisastanevica@gmail.com>
2025-02-28 10:29:29 +0200

    masivi 28.02.25

---
+++ 28.02.2025/masivi.js
@@ -0,0 +1,27 @@
+/*
+let arr = [1,2,3,4,5];
+let arrlastitem = arr.length - 1;
+
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[arrlastitem]);
+
+arr.push(6);
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+console.log (arr.pop());
+console.log(arr);
+*/
+
+let arr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+
+
+for (let i = arr2.length -1 ; i >= 0 ; i--)
+{
+    console.log(arr2[i]);
+}
